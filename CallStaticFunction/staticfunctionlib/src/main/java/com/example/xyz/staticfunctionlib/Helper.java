b6450a8b635a96f615a8c43d6aece24b1ca4791d
David <david@bluewaytech.com>
2016-08-08 14:19:16 +0800

    Added CallStaticFunction project.

---
+++ CallStaticFunction/staticfunctionlib/src/main/java/com/example/xyz/staticfunctionlib/Helper.java
@@ -0,0 +1,18 @@
+package com.example.xyz.staticfunctionlib;
+
+import android.util.Log;
+
+/**
+ * Created by blueway on 8/8/2016.
+ */
+
+public class Helper {
+    /**
+     * This function demonstrates the simplet function to be called inside Unity
+     */
+    public static void DoSthInAndroid() {
+        
+        Log.i("Unity", "Hi, Sth is done in Android");
+    }
+}
+
